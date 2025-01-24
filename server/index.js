18b828f8d9d50cbc5a6dfcff2bb1b28cb6ba5df3
mhmadiab <151012750+mhmadiab@users.noreply.github.com>
2025-01-24 19:36:23 +0200

    updating backend

--- server/index.js
+++ server/index.js
@@ -13,7 +13,8 @@ const itemRoutes = require("./routes/itemRoutes")
 const app = express()
 
 app.use(express.json());
-app.use(cors())
+// app.use(cors())
+app.use(cors({ origin: "https://internship-preparation-tasks-1.onrender.com" }));
 
 connectDB()
 
