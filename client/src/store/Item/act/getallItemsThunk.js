b13f56445a12f5997a6bf89bed2e0acf48ed8ce1
mhmadiab <151012750+mhmadiab@users.noreply.github.com>
2025-01-24 17:41:41 +0200

    frontend 2

--- client/src/store/Item/act/getallItemsThunk.js
+++ client/src/store/Item/act/getallItemsThunk.js
@@ -1,9 +1,10 @@
 import { createAsyncThunk } from "@reduxjs/toolkit"
 
-const host = process.env.BACKEND_API 
+
 
 const getAllItems = createAsyncThunk("item/getAllItems", async(_, ThunkAPI)=>{
     const {rejectWithValue} = ThunkAPI
+    const host = process.env.BACKEND_API || "http://localhost:4000"
     try {
         const response = await fetch(`${host}/api/item/allitems`)
   
